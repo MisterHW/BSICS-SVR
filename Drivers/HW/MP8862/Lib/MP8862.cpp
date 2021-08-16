eb0d119216cbafdcc16edbb04625c4c2e605d5ca
helge <helgewurst@web.de>
2021-08-16 22:14:02 +0200

    modify fixed-point calculations from int-part to rounding behavior

--- Drivers/HW/MP8862/Lib/MP8862.cpp
+++ Drivers/HW/MP8862/Lib/MP8862.cpp
@@ -52,10 +52,10 @@ bool MP8862::setEnable(bool soft_EN) {
 }
 
 bool MP8862::setCurrentLimit_mA(uint16_t current_mA) {
-    if(current_mA > 4049){ // highest permitted value producing int part 80
-        current_mA = 4049; // 4.0 A max (I_LIM = 0x50)
+    if(current_mA > 4025){ // highest permitted value producing int part 80
+        current_mA = 4025; // 4.0 A max (I_LIM = 0x50)
     }
-    uint32_t limit_raw = (current_mA * 1311) >> 16; // * 0.02 (~ 1311/65536)
+    uint32_t limit_raw = (current_mA * 1311 + (24 * 1311)) >> 16; // * 0.02 (~ 1311/65536)
     uint8_t tmp = limit_raw & 0x7F;
     return write( MP8862_REG_IOUT_LIM , &tmp, 1 );
 }
@@ -69,11 +69,11 @@ bool MP8862::readCurrentLimit_mA(uint16_t &current_mA) {
 }
 
 bool MP8862::setVoltageSetpoint_mV(uint16_t voltage_mV) {
-    if(voltage_mV > 20485){ // highest permitted value producing int part 2047
-        voltage_mV = 20485; // 20.47 V max (VOUT = 2047)
+    if(voltage_mV > 20480){ // highest permitted value producing int part 2047
+        voltage_mV = 20480; // 20.47 V max (VOUT = 2047)
     }
     uint8_t tmp[2];
-    uint32_t voltage_raw = (voltage_mV * 819) >> 13; // * 0.1 (~ 819/8192)
+    uint32_t voltage_raw = (voltage_mV * 819 + (5 * 819)) >> 13; // * 0.1 (~ 819/8192)
     tmp[0] = voltage_raw & 0x07; // VOUT_L value
     tmp[1] = (voltage_raw >> 3) & 0xFF; // VOUT_H value
     return write( MP8862_REG_VOUT_L , tmp, 2 );
