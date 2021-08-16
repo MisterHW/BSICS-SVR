be1b3f3f470b87f3da62417e6aecb4f085a873bc
helge <helgewurst@web.de>
2021-08-16 16:46:44 +0200

    annotate header definitions with undocumented register info

--- Drivers/HW/MP8862/Lib/MP8862.h
+++ Drivers/HW/MP8862/Lib/MP8862.h
@@ -15,13 +15,15 @@ enum MP8862_address {
     MP8862_ADDR_0x6F = 0x6F, // ADD = 0.75 .. 1.00 * VCC
 };
 
+// Register bits (Handling of "Reserved" bits is unclear, writing as "0" seems plausible, however they can read as 0 or 1 unless explicitly specified (see 0x06).
+
 enum MP8862_REG_VOUT_GO_bits {
     MP8862_GO_BIT      = 1 << 0 , // Set bit to 1 to engage voltage change after updating VOUT_L, VOUT_H registers. Resets to 0 when new setpoint is applied.
     MP8862_PG_DELAY_EN = 1 << 1 , // When PG_DELAY_EN D[1] is 1, PG experiences a 100μs rising delay. (default 0)
 };
 
 enum MP8862_REG_CTL1_bits {
-    MP8862_FREQ_500kHz    = 0x0 << 2, // switching frequency (default)
+    MP8862_FREQ_00_500kHz = 0x0 << 2, // switching frequency (default)
     // MP8862_FREQ bits 01, 10, 11 : reserved
     MP8862_MODE           = 1 << 4 , // switching mode: 0: Enables auto PFM/PWM mode, 1: Sets forced PWM mode (default)
     MP8862_DISCH_EN       = 1 << 5 , // 1: Output discharge function during EN or VIN shutdown (default)
@@ -31,6 +33,7 @@ enum MP8862_REG_CTL1_bits {
 };
 
 enum MP8862_REG_CTL2_bits {
+    // MP8862_REG_CTL2[3:0] : reserved
     MP8862_SS_00_300us           = 0x0 << 4 , // output start-up soft-start timer
     MP8862_SS_01_500us           = 0x1 << 4 ,
     MP8862_SS_10_700us           = 0x2 << 4 ,
@@ -67,6 +70,7 @@ enum MP8862_REG_INT_MASK{
     MP8862_OTP_MSK = 1 << 4 , // Masks off the OTP alert. Setting  MSK to 1 disables this interrupt source.
 };
 
+// MP8862 Revision 1 register map
 enum MP8862_register {
     MP8862_REG_VOUT_L    = 0x00, // R[2:0] : VOUT[2:0] - default 0x04
     MP8862_REG_VOUT_H    = 0x01, // R[7:0] : VOUT[10:3] - default 0x3E ( 0x3E << 3 | 0x04 = 500 decimal = 5.00V )
@@ -74,17 +78,20 @@ enum MP8862_register {
     MP8862_REG_IOUT_LIM  = 0x03, // R[6:0] : output current limit threshold (0 .. 4 A (ROC 21.5k : 50 mA steps) - defaut 0x3C = 3.0 A
     MP8862_REG_CTL1      = 0x04, // R[7] : EN, R[6]: HICCUP_OCP_OVP, R[5] : DISCHG_EN, R[4] : MODE, R[3:2]: FREQ - default 0xF0
     MP8862_REG_CTL2      = 0x05, // R[7:6] : LINE_DROP_COMP, R[5:4] : SS - default 0x30
-    // 0x06 .. 0x08 : Reserved
+    // 0x07 .. 0x08 : Reserved
+    MP8862_UNK_0x06      = 0x06, // 0x06 [7:1] is denoted "Reserved, ALL “0”" , [0] is denoted "Reserved", structurally an odd remnant. (variable)
+    MP8862_UNK_0x07      = 0x07, // undocumented (variable)
+    MP8862_UNK_0x08      = 0x08, // undocumented (variable)
     MP8862_REG_STATUS    = 0x09, // R[7] : PG, R[6] : OTP, R[5] : OTW, R[4] : CC_CV (instantaneous values)
     MP8862_REG_INTERRUPT = 0x0A, // R[7] : OTEMPP_ENTER, R[6] : OT_WARNING_ENTER, R[5] : OC_ENTER, R[4] : OC_RECOVER, R[3]: UVP_FALLING, R[2] : OTEMP_EXIT, R[1] : OT_WARNING_EXIT, R[0] : PG_RISING
                                  // Write 0xFF to MP8862_REG_INTERRUPT to reset the interrupt (ALT) pin state.
     MP8862_REG_INT_MASK  = 0x0B, // R[4] : OTPMSK, R[3] : OTWMSK, R[2] : OC_MSK, R[1] : UVP_MSK, R[0] : PG_MSK
     MP8862_REG_ID1       = 0x0C, // 0x00: standard MP8862-0000Q, 0x01: MP8862-0001 part number etc.
-    MP8862_UNK_0x0D      = 0x0D, // undocumented
+    MP8862_UNK_0x0D      = 0x0D, // undocumented (0x30)
     // 0x0E .. 0x26 : unused / undocumented
-    MP8862_REG_MFR_ID    = 0x27, // Manufacturer ID : 0x09
-    MP8862_REG_DEV_ID    = 0x28, // Device ID       : 0x58
-    MP8862_REG_IC_REV    = 0x29, // IC revision     : 0x01
+    MP8862_REG_MFR_ID    = 0x27, // Manufacturer ID (0x09)
+    MP8862_REG_DEV_ID    = 0x28, // Device ID       (0x58)
+    MP8862_REG_IC_REV    = 0x29, // IC revision     (0x01)
     // 0x40 .. 0x4F : ROM defaults? / unused
     MP8862_UNK_0x40      = 0x40, // undocumented (0x04)
     MP8862_UNK_0x41      = 0x41, // undocumented (0x3E)
